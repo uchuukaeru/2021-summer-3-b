0a790598c73f0a3ae8fe4c685e01e786f0852618
uchuukaeru <hirata.motoki@gmail.com>
2021-08-30 14:35:59 +0900

    deno fmtを実行

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-import {Server} from "https://js.sabae.cc/Server.js";
-import {jsonfs} from "https://js.sabae.cc/jsonfs.js";
+import { Server } from "https://js.sabae.cc/Server.js";
+import { jsonfs } from "https://js.sabae.cc/jsonfs.js";
 
-const boardfn="data/board.json";
+const boardfn = "data/board.json";
 
-let board=jsonfs.read(boardfn)||[];
+let board = jsonfs.read(boardfn) || [];
 
-class MyServer extends Server{
-    async api(path,req){
-        if (path == "/api/add") {
-            console.log("call badd");
-            board.push(req);
-            jsonfs.write(boardfn,board);
-            return "ok";
-        } else if (path == "/api/list") {
-            console.log("call list")
-            return board;
-        }
+class MyServer extends Server {
+  async api(path, req) {
+    if (path == "/api/add") {
+      console.log("call badd");
+      board.push(req);
+      jsonfs.write(boardfn, board);
+      return "ok";
+    } else if (path == "/api/list") {
+      console.log("call list");
+      return board;
     }
+  }
 }
 
 new MyServer(8001);
