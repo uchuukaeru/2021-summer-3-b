ecfc199c2d7d2c5e87e48c8cf7d4cefb34f81996
uchuukaeru <hirata.motoki@gmail.com>
2021-08-30 13:58:00 +0900

    掲示板の設定
    掲示板用スクリプトの追加

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import {Server} from "https://js.sabae.cc/Server.js";
+import {jsonfs} from "https://js.sabae.cc/jsonfs.js";
+
+const boardfn="data/board.json";
+
+let board=jsonfs.read(boardfn)||[];
 
 class MyServer extends Server{
-
+    async api(path,req){
+        if (path == "/api/add") {
+            console.log("call badd");
+            board.push(req);
+            jsonfs.write(boardfn,board);
+            return "ok";
+        } else if (path == "/api/list") {
+            console.log("call list")
+            return board;
+        }
+    }
 }
 
 new MyServer(8001);
