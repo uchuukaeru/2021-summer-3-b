15a8e8ef05068d121c0a5bad36ad5d5771dd6f21
uchuukaeru <hirata.motoki@gmail.com>
2021-08-31 14:51:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,35 +2,34 @@ import { Server } from "https://js.sabae.cc/Server.js";
 import { jsonfs } from "https://js.sabae.cc/jsonfs.js";
 import { WsServer } from "./ws/wsServer.js"
 
-const boardfn = "data/board.json";
-const userfn = "data/user.json"
-
-let board = jsonfs.read(boardfn) || [];
+const userfn = "data/users.json"
 let user = jsonfs.read(userfn) || [];
 
 class MyServer extends Server {
   async api(path, req) {
     if(path=="/api/login"){
-        //ログイン用API
-        //call:("api/login",{ID,pass}),return:{name,session}
-        console.log("call login");
-        let u=null;
-        console.log("id :",req.ID)
-        for(const d in user){
-          console.log(user[d].ID);
-          if(user[d].ID==req.ID){
-            u=d;
-            break;
-          }
+      //ログイン用API
+      //call:("api/login",{ID,pass}),return:{name,session}
+      console.log("call login");
+      let u=null;
+      console.log("id :",req.ID)
+      for(const d in user){
+        console.log(user[d].ID);
+        if(user[d].ID==req.ID){
+          u=d;
+          break;
         }
-        if(!u) return "not found";
-        if(user[u].pass!=req.pass)return null;
-        const res={name:user[u].name,session:user[u].session};
-        //console.log(res);
-        user[u].is_active=true;
-        jsonfs.write(userfn,user);
-        return res;
       }
+      if(!u) return "not found";
+      if(user[u].pass!=req.pass)return null;
+      const res={name:user[u].name,session:user[u].session};
+      //console.log(res);
+      user[u].is_active=true;
+      jsonfs.write(userfn,user);
+      return res;
+    }else if(path=="/api/register"){
+      
+    }
   }
 }
 
