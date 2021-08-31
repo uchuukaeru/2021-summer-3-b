017bb4b41808fea5c33a2cbbf9251552f21ef17e
haryoiro <55312590+haryoiro@users.noreply.github.com>
2021-08-31 12:53:13 +0900

    リクエストの部分のみ別ファイルへ分離

---
+++ static/js/services/services.js
@@ -0,0 +1,20 @@
+
+const login = async (id, password) => {
+  const data = await fetchJSON("/api/login", {
+    ID: id,
+    password,
+  })
+  return data
+}
+
+const getFriends = async (id, session) => {
+  await fetchJSON("/api/getFriends", {
+    ID: id,
+    session,
+  })
+}
+
+export {
+  login,
+  getFriends,
+}
