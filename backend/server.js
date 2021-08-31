35905c7ad18c975f7d56a8aff2927d7d005ef17e
miyamoto-kentaro <kentaro.miyamoto1001@gmail.com>
2021-08-31 17:28:08 +0900

    vue

---
+++ backend/server.js
@@ -0,0 +1,68 @@
+import { Server } from "https://js.sabae.cc/Server.js";
+import { jsonfs } from "https://js.sabae.cc/jsonfs.js";
+// import * as bcrypt from "https://deno.land/x/crypt@v0.1.0/bcrypt.ts";
+
+const userfn = "data/users.json";
+let user = jsonfs.read(userfn) || [];
+
+class MyServer extends Server {
+  async api(path, req) {
+    if (path == "/api/login") {
+      //ログイン用API
+      //call:("api/login",{ID,pass}),return:{name,session}
+      console.log("call login");
+      let u = null;
+      console.log("id :", req.ID);
+      for (const d in user) {
+        console.log("cycle");
+        console.log(user[d].ID);
+        if (user[d].ID == req.ID) {
+          u = d;
+          break;
+        }
+      }
+      if (!u) return "not found";
+      // const result = bcrypt.compareSync(user[u].pass, req.pass);
+      if (user[u].pass != req.pass) return null;
+      const res = { name: user[u].name, session: user[u].session };
+      //console.log(res);
+      user[u].is_active = true;
+      jsonfs.write(userfn, user);
+      return res;
+    } else if (path == "/api/register") {
+      //ユーザ登録用API
+      //call:("api/register",{name,pass}),return:"ok"
+      console.log("call register");
+      let ses_array = [];
+      for (const d of user) {
+        ses_array.push(d.session);
+      }
+      console.log(ses_array);
+      let ses = 0;
+      while (1) {
+        ses = Math.random();
+        if (!ses_array.includes(ses)) break;
+      }
+      const id = user.length + 1;
+      const item = {
+        ID: id,
+        name: req.name,
+        pass: req.pass,
+        session: ses,
+        is_active: true,
+        fitness: [],
+        friend_ID: [],
+      };
+      user.push(item);
+      jsonfs.write(userfn, user);
+      const res = {
+        ID: id,
+        session: ses,
+      };
+      return res;
+    } else if (path == "api/get_active") {
+    }
+  }
+}
+
+new MyServer(8001);
--- static/js/index.js
+++
@@ -1,7 +0,0 @@
-import { loginPage } from './login.js'
-import { mainPage } from "./main.js"
-
-
-window.onload = () => {
-    loginPage()
-}
