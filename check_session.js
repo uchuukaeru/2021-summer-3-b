7b213951431af68341a158562040042fbe7f8315
haryoiro <55312590+haryoiro@users.noreply.github.com>
2021-09-01 13:52:14 +0900

    Update check_session.js

--- check_session.js
+++ check_session.js
@@ -5,20 +5,14 @@ let user = jsonfs.read(userfn) || [];
 
 function check_session(item){
     console.log("call function check_session")
-    let u=null;
 
-    console.log("id :",item.ID);
-    for(const d in user){
-        console.log(user[d].ID);
-        if(user[d].ID==item.ID){
-            console.log(d);
-            u=d;
-            break;
-        }
-    }
-    if(!u) return "not found";
-    if(user[u].session!=item.session)return "session error";
-    return u;
+    const checked = user.find((a) => {
+        return String(a.ID) == String(item.ID) && String(a.session) == String(item.session)
+    })
+    
+    if (!checked) return "session error";
+
+    return checked;
 }
 
 function login_check(item){
