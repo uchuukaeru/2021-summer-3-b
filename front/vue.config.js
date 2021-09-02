0d769d76fdf6f23d6444ba540eea999067ceb780
Haryoiro <mizusecocolte@gmail.com>
2021-09-02 12:44:57 +0900

    ビルド時、ファイル名にハッシュ値を付けないよう変更

--- front/vue.config.js
+++ front/vue.config.js
@@ -5,5 +5,15 @@ module.exports = {
       maxAssetSize: 270000,
       maxEntrypointSize: 700000,
     },
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  },
+  css: {
+    extract: {
+      filename: '[name].css',
+      chunkFilename: '[name].css'
+    },
   },
 };
